6a78c1c0b754fd7a8579469bfbd50a6f74501106
Sasha <alecsand.unit@gmail.com>
2024-03-20 16:50:25 +0200

    Add project files.

---
+++ UDPClient/Client.cpp
@@ -0,0 +1,121 @@
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <ws2tcpip.h>
+#include <windows.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+#pragma comment (lib, "Ws2_32.lib")
+
+#define DEFAULT_BUFLEN 4096
+
+#define SERVER_IP "127.0.0.1"
+#define DEFAULT_PORT "8888"
+
+SOCKET client_socket; 
+static int i = 0;
+
+DWORD WINAPI Sender(void* param)    
+{
+    cout << "MENU: " << endl;   
+    cout << "1. hamburger - 5$ (7 sec)" << endl;
+    cout << "2. nuggets - 3$ (5 sec.)" << endl; 
+    cout << "3. potato - 3$ (6 sec)" << endl;
+    cout << "4. cola - 1$ (3 sec)" << endl;
+
+    while (true) {
+
+        string query;
+        string payment;
+        string order;   
+
+        cout << endl;
+        cout << "Write your order: " << endl;     
+        getline(cin, query);   
+        cout << "Make payment: ";
+        getline(cin, payment);
+
+        order = query + " " + payment; 
+        send(client_socket, order.c_str(), order.size(), 0);    
+    }
+}
+
+DWORD WINAPI Receiver(void* param)  
+{
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE); 
+    cout << endl;   
+    while (true) {
+        char response[DEFAULT_BUFLEN];
+        int result = recv(client_socket, response, DEFAULT_BUFLEN, 0);  
+        response[result] = '\0'; 
+        cout << response << endl; 
+
+    }
+}
+
+int main()
+{
+    // обработчик закрытия окна консоли
+    //SetConsoleCtrlHandler((PHANDLER_ROUTINE)ExitHandler, true);
+
+    system("title Client");
+
+    // initialize Winsock
+    WSADATA wsaData;
+    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0) {
+        printf("WSAStartup failed with error: %d\n", iResult);
+        return 1;
+    }
+    addrinfo hints = {};
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    // разрешить адрес сервера и порт
+    addrinfo* result = nullptr;
+    iResult = getaddrinfo(SERVER_IP, DEFAULT_PORT, &hints, &result);
+    if (iResult != 0) {
+        printf("getaddrinfo failed with error: %d\n", iResult);
+        WSACleanup();
+        return 2;
+    }
+
+    addrinfo* ptr = nullptr;
+    // пытаться подключиться к адресу, пока не удастся
+    for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
+        // создать сокет на стороне клиента для подключения к серверу
+        client_socket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+
+
+        if (client_socket == INVALID_SOCKET) {
+            printf("socket failed with error: %ld\n", WSAGetLastError());
+            WSACleanup();
+            return 3;
+        }
+
+        // connect to server
+        iResult = connect(client_socket, ptr->ai_addr, (int)ptr->ai_addrlen);
+        if (iResult == SOCKET_ERROR) {
+            closesocket(client_socket);
+            client_socket = INVALID_SOCKET;
+            continue;
+        }
+        break;
+    }
+
+    freeaddrinfo(result);
+
+    if (client_socket == INVALID_SOCKET) {
+        printf("Unable to connect to server!\n");
+        WSACleanup();
+        return 5;
+    }
+
+    CreateThread(0, 0, Sender, 0, 0, 0);
+    CreateThread(0, 0, Receiver, 0, 0, 0);
+
+    Sleep(INFINITE);
+}
---
+++ UDPExample/Server.cpp
@@ -0,0 +1,191 @@
+#include <winsock2.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream> 
+using namespace std;
+
+#define MAX_CLIENTS 30	
+#define DEFAULT_BUFLEN 4096
+
+#pragma comment(lib, "ws2_32.lib") // Winsock library
+#pragma warning(disable:4996) // отключаем предупреждение _WINSOCK_DEPRECATED_NO_WARNINGS
+
+SOCKET server_socket;
+
+vector<string> history;
+vector <string> menu;	
+int main() {
+	system("title Server");
+
+	menu.push_back("hamburger");	
+	menu.push_back("nuggets");	
+	menu.push_back("potato");	
+	menu.push_back("cola");	
+
+	puts("Start server... DONE.");
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+		printf("Failed. Error Code: %d", WSAGetLastError());
+		return 1;
+	}
+
+	// create a socket
+	if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+		printf("Could not create socket: %d", WSAGetLastError());
+		return 2;
+	}
+	// puts("Create socket... DONE.");
+
+	// prepare the sockaddr_in structure
+	sockaddr_in server;
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons(8888);
+
+	// bind socket
+	if (bind(server_socket, (sockaddr*)&server, sizeof(server)) == SOCKET_ERROR) {
+		printf("Bind failed with error code: %d", WSAGetLastError());
+		return 3;
+	}
+
+	// слушать входящие соединения
+	listen(server_socket, MAX_CLIENTS);
+
+	// принять и входящее соединение
+	puts("Server is waiting for incoming connections...\nPlease, start one or more client-side app.");
+
+	// размер нашего приемного буфера, это длина строки
+	// набор дескрипторов сокетов
+	// fd means "file descriptors"
+	fd_set readfds; // https://docs.microsoft.com/en-us/windows/win32/api/winsock/ns-winsock-fd_set
+	SOCKET client_socket[MAX_CLIENTS] = {};
+
+	while (true) {
+		// очистить сокет fdset
+		FD_ZERO(&readfds);
+
+		// добавить главный сокет в fdset
+		FD_SET(server_socket, &readfds);
+
+		// добавить дочерние сокеты в fdset
+		for (int i = 0; i < MAX_CLIENTS; i++) 
+		{
+			SOCKET s = client_socket[i];
+			if (s > 0) {
+				FD_SET(s, &readfds);
+			}
+		}
+
+		// дождитесь активности на любом из сокетов, тайм-аут равен NULL, поэтому ждите бесконечно
+		if (select(0, &readfds, NULL, NULL, NULL) == SOCKET_ERROR) {
+			printf("select function call failed with error code : %d", WSAGetLastError());
+			return 4;
+		}
+
+		// если что-то произошло на мастер-сокете, то это входящее соединение
+		SOCKET new_socket; // новый клиентский сокет
+		sockaddr_in address;
+		int addrlen = sizeof(sockaddr_in);
+
+		if (FD_ISSET(server_socket, &readfds)) {
+			if ((new_socket = accept(server_socket, (sockaddr*)&address, &addrlen)) < 0) {
+				perror("accept function error");
+				return 5;
+			}
+
+			for (int i = 0; i < history.size(); i++)
+			{
+				cout << history[i] << "\n";
+				send(new_socket, history[i].c_str(), history[i].size(), 0);
+			}
+			
+			// информировать серверную сторону о номере сокета - используется в командах отправки и получения
+			printf("New connection, socket fd is %d, ip is: %s, port: %d\n", new_socket, inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+
+			// добавить новый сокет в массив сокетов
+			for (int i = 0; i < MAX_CLIENTS; i++) {
+				if (client_socket[i] == 0) {
+					client_socket[i] = new_socket;
+					printf("Adding to list of sockets at index %d\n", i);
+					break;
+				}
+			}
+		}
+
+		// если какой-то из клиентских сокетов отправляет что-то
+		int time = 0;
+		int price = 0;	
+		for (int i = 0; i < MAX_CLIENTS; i++)
+		{
+			SOCKET s = client_socket[i];
+			// если клиент присутствует в сокетах чтения
+			if (FD_ISSET(s, &readfds))
+			{
+				// получить реквизиты клиента
+				getpeername(s, (sockaddr*)&address, (int*)&addrlen);
+
+				// проверьте, было ли оно на закрытие, а также прочитайте входящее сообщение
+				// recv не помещает нулевой терминатор в конец строки (в то время как printf %s предполагает, что он есть)
+
+				char client_message[DEFAULT_BUFLEN];	
+
+				int client_message_length = recv(s, client_message, DEFAULT_BUFLEN, 0);
+				client_message[client_message_length] = '\0';
+				string check_exit = client_message;	
+						
+				for (size_t j = 0; j < menu.size(); ++j) {		
+					if (check_exit.find(menu[j]) != -1) {		
+						if (menu[j] == "hamburger") {
+							time += 7;
+							price += 5;
+						}
+						if (menu[j] == "nuggets") {
+							time += 5;
+							price += 3;
+						}
+						if (menu[j] == "potato") {
+							time += 6;
+							price += 3;	
+						}
+						if (menu[j] == "cola") {
+							time += 3;	
+							price += 1;	
+						}
+					}
+				}
+
+				size_t last_space_index = check_exit.find_last_of(' ');	
+				string last_word = check_exit.substr(last_space_index + 1);		
+				int client_payment = stoi(last_word);	
+				
+				if(price > client_payment){
+					int difference = price - client_payment;	
+					string not_enough_to_pay = to_string(difference);	
+					string erroror_messege = "You are missing $" + not_enough_to_pay + " to pay for your order";		
+					send(client_socket[i], erroror_messege.c_str(), client_message_length, 0);	
+				}
+
+				if (check_exit == "off")
+				{
+					cout << "Client #" << i << " is off\n";
+					client_socket[i] = 0;
+				}	
+				
+				string temp = client_message;
+				// temp += "\n";
+				history.push_back(temp);
+
+				
+				/*for (int i = 0; i < MAX_CLIENTS; i++) {
+					if (client_socket[i] != 0) {
+						send(client_socket[i], client_message, client_message_length, 0);
+					}
+				}*/
+
+			}
+		}
+	}
+
+	WSACleanup();
+}
